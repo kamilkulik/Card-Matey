d4165503411742764485bb1f76f00ea73d0e1fa0
Kamil <kulikkamil@gmail.com>
2020-11-10 09:03:43 +0100

    firebase connection

--- BE/index.js
+++ BE/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = 3700
+
+const admin = require('firebase-admin')
+const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://card-matey.firebaseio.com',
+})
+
+const db = admin.database()
+if (db) console.log('Firebase connected')
+else console.log('connection error')
+const cardRef = db.ref('cards')
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
