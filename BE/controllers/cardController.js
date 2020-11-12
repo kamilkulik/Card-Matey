c4de5f3466b2b575e93d7340ed47a6acb191c50f
Kamil <kulikkamil@gmail.com>
2020-11-12 22:37:16 +0100

    read all route

--- BE/controllers/cardController.js
+++ BE/controllers/cardController.js
@@ -1,14 +1,26 @@
+const { firestore } = require('firebase-admin')
 const database = require('../config/database')
 
 const db = database.getDb()
 const docRef = db.collection('cards')
 
 exports.createCard = async function (req, res, next) {
-  const docName = req.body.cardName
-  await docRef.doc(docName).set({
-    first: req.body.first,
-    last: req.body.last,
-    profession: req.body.job,
+  // const docName = req.body.cardName // name of the document that will be put in collection
+  // const cardAttributes = Object.entries(req.body).filter((el) => el[0] !== 'cardName')
+  // const cardAttributesObject = Object.fromEntries(cardAttributes)
+
+  // allow setting empty documents
+  const date = new Date()
+  await docRef.add({
+    // .add() method adds to collection a document whose name is a unique ID
+    timestamp: firestore.Timestamp.fromDate(date),
+    ...req.body,
   })
-  res.status(200).send(`${docName} successfully added to Database`)
+  res.status(200).send(`Card successfully added to Database`)
+}
+
+exports.readCards = async function (req, res, next) {
+  const snapshot = await docRef.get()
+  const data = snapshot.docs.map((doc) => doc.data())
+  res.status(200).json(data)
 }
--- BE/routes/router.js
+++ BE/routes/router.js
@@ -5,4 +5,6 @@ const businessCard = require('../controllers/cardController')
 
 router.post('/card/add', catchErrors(businessCard.createCard))
 
+router.get('/cards/all', catchErrors(businessCard.readCards))
+
 module.exports = router
