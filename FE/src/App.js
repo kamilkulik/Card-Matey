78962f9727e64c68c7695873f673e700b3fc0107
Kamil <kulikkamil@gmail.com>
2020-11-15 00:01:18 +0100

    getCard custom hook improvement

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import useGetCards from './hooks/getCards'
 
 function App() {
-  const res = useGetCards()
-  if (res.status === 'fetched') {
-    console.log(res)
-  }
-  return <div className='app-wrapper'>Hello!</div>
+  const { status, data, error } = useGetCards()
+  return (
+    <React.Fragment>
+      {status === 'fetched' && (
+        <div className='app-wrapper'>Hello! There are: {data.length} business cards stored</div>
+      )}
+    </React.Fragment>
+  )
 }
 
 export default App
--- FE/src/hooks/getCards.js
+++ FE/src/hooks/getCards.js
@@ -1,14 +1,14 @@
 import { useEffect, useReducer } from 'react'
 import axios from 'axios'
 
-const initialState = { status: 'idle', data: '', erro: null }
+const initialState = { status: 'idle', data: '', error: null }
 
 function reducer(state, action) {
   switch (action.type) {
     case 'FETCHING ':
       return { ...initialState, status: 'fetching' }
     case 'FETCHED':
-      return { ...initialState, status: 'fetched', payload: action.payload }
+      return { ...initialState, status: 'fetched', data: action.payload }
     case 'FETCH_ERR':
       return { ...initialState, status: 'error', error: action.payload }
     default:
@@ -25,7 +25,7 @@ const useGetCard = (card) => {
       .get('http://localhost:3700/card/')
       .then((res) => {
         console.log(res)
-        dispatch({ type: 'FETCHED', payload: res })
+        dispatch({ type: 'FETCHED', payload: res.data })
       })
       .catch((err) => {
         console.log(err)
