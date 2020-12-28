ad82eae59b05a4a46fafabb87847628dc9b4efb0
Kamil <kulikkamil@gmail.com>
2020-12-29 00:04:09 +0100

    loading card themes from the backend

--- FE/src/components/businessCard/CardContainer.js
+++ FE/src/components/businessCard/CardContainer.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import sizeMe from 'react-sizeme'
 import PropTypes from 'prop-types'
-// import ThemeContext from '../../ThemeContext'
+import ThemeContext from '../../ThemeContext'
 import useDynamicFont from '../../hooks/useDynamicFont'
 import canvasLogos from '../canvas/CanvasLogos'
-import cardPatterns from './CardPatterns'
 import Canvas from '../canvas/Canvas'
 
 const CardContainer = ({
@@ -13,14 +12,16 @@ const CardContainer = ({
   spec: { logo: logoProp = 'squares', theme: themeProp = 'none', colour: colourProp = 'black' },
 }) => {
   useDynamicFont(12, width)
-  // const cachedThemes = useContext(theme)
+  const cachedThemes = React.useContext(ThemeContext)
+  const colourLessTheme = cachedThemes.find(((pattern) => pattern.name === themeProp)).pattern
+  const theme = colourLessTheme.replace('black', colourProp)
 
-  const theme = cardPatterns(colourProp).find((pattern) => pattern.name === themeProp)
-    .pattern
+  const style = { backgroundImage: theme }
+
   const drawFunction = canvasLogos.find((logo) => logo.name === logoProp).draw
 
   return (
-    <div className='presentation' style={{ backgroundImage: theme }}>
+    <div className='presentation' style={style}>
       <div className='presentation__canvas'>
         <Canvas draw={drawFunction} key={logoProp} />
       </div>
@@ -45,3 +46,9 @@ CardContainer.propTypes = {
 }
 
 export default sizeMe()(CardContainer)
+
+/*
+url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='LightSlateGrey' fill-opacity='0.5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")
+
+url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill-opacity='0.5' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E fill='LightSlateGrey'")
+*/
--- FE/src/components/previewBox/ThemePreview.js
+++ FE/src/components/previewBox/ThemePreview.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import themes from '../businessCard/CardPatterns'
+import ThemeContext from '../../ThemeContext'
 import PreviewBox from './PreviewBox'
 
-const ThemePreview = ({ savedTheme, handleSelect }) => (
-  <PreviewBox>
-    {themes().map((theme) => (
-      <div
-        className={
-        savedTheme === theme.name ? 'previewBox__wrapper active' : 'previewBox__wrapper'
-      }
-        onClick={() => handleSelect('theme', theme.name)}
-        key={theme.name}
-        style={{ backgroundImage: theme.pattern }}
-        role='button'
-        aria-label={theme.name}
-        tabIndex='0'
-      />
-    ))}
-  </PreviewBox>
-)
+const ThemePreview = ({ savedTheme, handleSelect }) => {
+  const cachedThemes = React.useContext(ThemeContext)
+
+  return (
+    <PreviewBox>
+      {cachedThemes.map((theme) => (
+        <div
+          className={
+          savedTheme === theme.name ? 'previewBox__wrapper active' : 'previewBox__wrapper'
+        }
+          onClick={() => handleSelect('theme', theme.name)}
+          key={theme.name}
+          style={{ backgroundImage: theme.pattern }}
+          role='button'
+          aria-label={theme.name}
+          tabIndex='0'
+        />
+      ))}
+    </PreviewBox>
+  )
+}
 
 ThemePreview.propTypes = {
   savedTheme: PropTypes.string.isRequired,
